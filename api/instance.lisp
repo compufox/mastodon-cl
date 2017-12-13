2b2487b55e221b2e17395dd28db6c3eed8794fe5
Zachary Epps <zactepps@gmail.com>
2017-12-13 02:09:50 -0500

    added gitignore added instance.lisp

---
+++ api/instance.lisp
@@ -0,0 +1,16 @@
+(in-package :mastodon.api)
+
+(defparameter *instance* "https://mastodon.social"
+  "the instance that we should use, defaults to mastodon.social")
+
+(defun get-instance ()
+  "get information about the current instance"
+  (decode-json-from-string
+   (masto--perform-request '(:get "instance"))))
+
+(defun set-instance (instance)
+  (setq *instance* (if (not (find "https://" instance))
+		       (concatenate 'string "https://" instance)
+		       instance))
+  (handler-case (get-instance)
+    (json:json-syntax-error () (error 'invalid-instance :reason "the instance url entered is not a valid mastodon instance"))))
