82b5ddf80c70b1680c59126bcd927971f5c9bc66
Zachary Epps <zactepps@gmail.com>
2017-12-13 02:14:34 -0500

    moved block/mute functions from accounts to blocks

--- api/blocks.lisp
+++ api/blocks.lisp
@@ -31,3 +31,33 @@
 			   ,(concatenate 'string
 					 "domain_blocks"
 					 "?domain=" domain))))
+
+(defun block-account (id)
+  (masto--perform-request `(:post
+			   ,(concatenate 'string
+					 "accounts/" id "/block"))))
+
+(defun unblock-account (id)
+  (masto--perform-request `(:post
+			   ,(concatenate 'string
+					 "accounts/" id "/unblock"))))
+
+(defun mute-account (id)
+  (masto--perform-request `(:post
+			   ,(concatenate 'string
+					 "accounts/" id "/mute"))))
+
+(defun unmute-account (id)
+  (masto--perform-request `(:post
+			   ,(concatenate 'string
+					 "accounts/" id "/unmute"))))
+
+(defun get-mutes (&key max-id since-id (limit 40))
+  (setq limit (min limit 80))
+  (decode-json-from-string
+   (masto--perform-request `(:get
+			    ,(concatenate 'string
+					  "mutes"
+					  "?limit=" (write-to-string limit)
+					  (if max-id (concatenate 'string "&max_id=" max-id))
+					  (if since-id (concatenate 'string "&since_id=" since-id)))))))
